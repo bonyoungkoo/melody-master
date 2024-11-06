f7ce7089d4ff2105f48712ab0af96bf7115c70ad
bonyoungkoo <gby1202@gmail.com>
2024-11-06 14:51:16 +0900

    경로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from '@/App';
+import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Rating, TextField, styled } from "@mui/material";
+import { Box, Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, styled } from "@mui/material";
 import { useRecoilValue } from "recoil";
 import { useNavigate } from "react-router-dom";
 import { useCallback, useState } from "react";
